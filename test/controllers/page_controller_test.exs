52b112b040da88ded09eabf6bb3fcee3c080806d
Donato Vianna <donatoaz@gmail.com>
2017-12-29 14:18:19 +0000

    Revert "initial commit"
    This reverts commit 6314301363bc096de2878644ba209540c2e4a37f.

--- test/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Rodes.PageControllerTest do
-  use Rodes.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
--- test/views/page_view_test.exs
+++
@@ -1,3 +0,0 @@
-defmodule Rodes.PageViewTest do
-  use Rodes.ConnCase, async: true
-end
